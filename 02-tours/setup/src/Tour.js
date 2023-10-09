a412bec4f441cdfa3ac68c55477c23d5e8fa32dc
Emmanuel Achimugu Ikeleji <ikeleji@gmail.com>
2023-10-09 19:28:02 +0100

    text

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, price, name, removeTour}) => {
+  const [readmore, setReadMore] = useState(false)
+  return (
+    <article className='single tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+          <h4>{name} </h4>
+          <h4 className='tour-price'>${price} </h4>
+        </div>
+        <p>{readmore ? info : `${info.substring(0, 300)}...`
+        } <button onClick={() => setReadMore(!readmore)}>
+            {readmore ? 'show less' : 'read more'}
+        </button>
+        </p>
+        <button className='delete-btn' onClick={()=> removeTour(id)}>Not Interested</button>
+      </footer>
+    </article>
+  )
 };
 
 export default Tour;
