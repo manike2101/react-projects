2da54180efab2c360a4a9eed4923a70ddf8ecc0f
Emmanuel Achimugu Ikeleji <ikeleji@gmail.com>
2023-10-09 20:40:31 +0100

    change the style

--- 01-birthday-reminder/setup/src/data.js
+++ 01-birthday-reminder/setup/src/data.js
@@ -1,35 +1,35 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Carla McBride',
+    age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Malcom Buckworth',
+    age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: 'Warren Devlin',
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: 'Fredrick O Hara',
+    age: 39,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: 'Janet Crane',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = ({id, image, info, price, name, removeTour}) => {
-  const [readmore, setReadMore] = useState(false)
+const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readmore, setReadMore] = useState(false);
   return (
-    <article className='single tour'>
+    <article className="single-tour">
       <img src={image} alt={name} />
       <footer>
-        <div className='tour-info'>
+        <div className="tour-info">
           <h4>{name} </h4>
-          <h4 className='tour-price'>${price} </h4>
+          <h4 className="tour-price">${price} </h4>
         </div>
-        <p>{readmore ? info : `${info.substring(0, 300)}...`
-        } <button onClick={() => setReadMore(!readmore)}>
-            {readmore ? 'show less' : 'read more'}
-        </button>
+        <p>
+          {readmore ? info : `${info.substring(0, 300)}...`}{" "}
+          <button onClick={() => setReadMore(!readmore)}>
+            {readmore ? "show less" : "read more"}
+          </button>
         </p>
-        <button className='delete-btn' onClick={()=> removeTour(id)}>Not Interested</button>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          Not Interested
+        </button>
       </footer>
     </article>
-  )
+  );
 };
 
 export default Tour;
